e19a02c685aa11d1ead0121510c9b9211fa26919
hongrui16 <hongrui_hust@foxmail.com>
2024-04-08 23:26:00 -0400

    update...

--- demo.py
+++ demo.py
@@ -30,6 +30,7 @@ import numpy as np
 import cv2
 import argparse
 import json
+import time
 
 from models import hmr, SMPL
 from utils.imutils import crop
@@ -67,15 +68,17 @@ def bbox_from_json(bbox_file):
     # make sure the bounding box is rectangular
     return center, scale
 
-def process_image(img_file, bbox_file, openpose_file, input_res=224):
+def process_image(ori_img, bbox_file = None, openpose_file = None, input_res=224, bbox = None):
     """Read image, do preprocessing and possibly crop it according to the bounding box.
     If there are bounding box annotations, use them to crop the image.
     If no bounding box is specified but openpose detections are available, use them to get the bounding box.
+    bbox: [x_min, y_min, x_max, y_max] 
     """
     normalize_img = Normalize(mean=constants.IMG_NORM_MEAN, std=constants.IMG_NORM_STD)
-    ori_img = cv2.imread(img_file)
     img = ori_img[:,:,::-1].copy() # PyTorch does not support negative stride at the moment
     if bbox_file is None and openpose_file is None:
+        if bbox is not None:
+            img = ori_img[bbox[1]:bbox[3], bbox[0]:bbox[2]]
         # Assume that the person is centerered in the image
         height = img.shape[0]
         width = img.shape[1]
@@ -100,11 +103,89 @@ def process_image(img_file, bbox_file, openpose_file, input_res=224):
     norm_img = normalize_img(img.clone())[None]
     return img, norm_img
 
-def run_demo(model, smpl, renderer, img_filepath, device, output_dir, bbox_file=None, openpose_file=None):
-    
-    img, norm_img = process_image(img_filepath, bbox_file, openpose_file, input_res=constants.IMG_RES)
+
+def yolo_detect_person(frame, yolo_model, device, vis_results=False):
+    """Use YOLO to detect a person in the frame."""
+    ori_frame = frame.copy()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # 转换颜色空间
+    frame = np.transpose(frame, (2, 0, 1))  # HWC到CHW
+    frame = np.expand_dims(frame, axis=0)  # 增加批次维度
+    frame = frame / 255.0  # 归一化
+    frame = torch.tensor(frame).float()  # 转换为torch tensor
+    frame = frame.to(device)  # 移动到GPU
+
+    # 进行检测
     with torch.no_grad():
-        pred_rotmat, pred_betas, pred_camera = model(norm_img.to(device))
+        results = yolo_model(frame)
+
+    for det in results.xyxy[0]:
+        if det[5] == 0:  # 在COCO数据集中，类别0通常是人
+            xmin, ymin, xmax, ymax = map(int, det[:4])
+            # cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
+            if vis_results:
+                cv2.rectangle(ori_frame, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
+    if vis_results:
+        # 显示结果
+        cv2.imshow('YOLOv5 Detection', frame)
+
+        if cv2.waitKey(1) == ord('q'):  # 按q退出
+            cv2.destroyAllWindows()
+
+    return [xmin, ymin, xmax, ymax]
+
+
+def cv2_ssd_detect_person(image, ssd_net, device, vis_results=False):
+    """Use YOLO to detect a person in the frame."""
+    h, w, _ = image.shape
+    # 将图片转换为模型输入的格式
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
+
+    # 输入数据并进行前向传播
+    ssd_net.setInput(blob)
+    detections = ssd_net.forward()
+
+
+
+        # 遍历检测结果
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        
+        # 筛选出高置信度的检测结果
+        if confidence > 0.2:
+            # 获取类别ID，如果检测到的是人，则class_id为15
+            class_id = int(detections[0, 0, i, 1])
+            
+            # 只处理人体检测结果
+            if class_id == 15:
+                # 计算边界框的坐标
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+
+                if vis_results:
+                    # 绘制边界框和置信度
+                    label = "{}: {:.2f}%".format("Person", confidence * 100)
+                    cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
+                    y = startY - 15 if startY - 15 > 15 else startY + 15
+                    cv2.putText(image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                    cv2.imshow("Output", image)
+
+                    if cv2.waitKey(0) == ord('q'):  # 按q退出
+                        cv2.destroyAllWindows()
+
+    return [startX, startY, endX, endY]
+
+
+
+def run_demo(hmr_model, smpl, renderer, img_filepath, device, output_dir, bbox_file=None, openpose_file=None, ssd_net=None):
+    
+    ori_img = cv2.imread(img_filepath) # BGR image
+    if ssd_net is not None:
+        start_time = time.time()
+        bbox = cv2_ssd_detect_person(ori_img.copy(), ssd_net, device, vis_results=False)
+        print('ssd detect time: ', time.time() - start_time)
+    img, norm_img = process_image(ori_img.copy(), bbox_file, openpose_file, input_res=constants.IMG_RES, bbox=bbox)
+    with torch.no_grad():
+        pred_rotmat, pred_betas, pred_camera = hmr_model(norm_img.to(device))
         pred_output = smpl(betas=pred_betas, body_pose=pred_rotmat[:,1:], global_orient=pred_rotmat[:,0].unsqueeze(1), pose2rot=False)
         pred_vertices = pred_output.vertices
         
@@ -139,38 +220,48 @@ def run_demo(model, smpl, renderer, img_filepath, device, output_dir, bbox_file=
     cv2.imwrite(shape_side_filepath, 255 * img_shape_side[:,:,::-1])
 
 def main(args):
+    
+    img_filepath = args.img
+    bbox_file = args.bbox
+    openpose_file = args.openpose
+    input_dir = args.inputdir
     output_dir = args.outdir
+    
     os.makedirs(output_dir, exist_ok=True)
     # args.checkpoint = r'C:\Users\hongr\Documents\GMU_research\computerVersion\hand_modeling\VIBE\data\vibe_data\spin_model_checkpoint.pth.tar'
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     
     # Load pretrained model
-    model = hmr(config.SMPL_MEAN_PARAMS).to(device)
+    hmr_model = hmr(config.SMPL_MEAN_PARAMS).to(device)
     checkpoint = torch.load(args.checkpoint)
-    model.load_state_dict(checkpoint['model'], strict=False)
+    hmr_model.load_state_dict(checkpoint['model'], strict=False)
+
+    # yolo_model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True).to(device)
+    # yolo_model.eval()
+
+    ssd_net = cv2.dnn.readNetFromCaffe('ssd_caffe/MobileNetSSD_deploy.prototxt',
+                               'ssd_caffe/MobileNetSSD_deploy.caffemodel')
+
 
     # Load SMPL model
     smpl = SMPL(config.SMPL_MODEL_DIR,
                 batch_size=1,
                 create_transl=False).to(device)
-    model.eval()
+    hmr_model.eval()
 
     # Setup renderer for visualization
     renderer = Renderer(focal_length=constants.FOCAL_LENGTH, img_res=constants.IMG_RES, faces=smpl.faces)
 
 
     # Preprocess input image and generate predictions
-    img_filepath = args.img
-    bbox_file = args.bbox
-    openpose_file = args.openpose
-    input_dir = args.inputdir
+
     if input_dir is not None:
         img_files = [os.path.join(input_dir, f) for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
         for img_file in img_files:
             print('Processing: ', img_file)
-            run_demo(model, smpl, renderer, img_file, device, output_dir, bbox_file, openpose_file)
+            run_demo(hmr_model, smpl, renderer, img_file, device, output_dir, bbox_file, openpose_file, ssd_net)
     elif img_filepath is not None:
-        run_demo(model, smpl, renderer, img_filepath, device, output_dir, bbox_file, openpose_file)
+        run_demo(hmr_model, smpl, renderer, img_filepath, device, output_dir, bbox_file, openpose_file, ssd_net)
     else:
         print('No input image provided. Please provide either --img or --inputdir argument.')
 
